a059c1225799f9b4bcce152fbf1b35908068b897
yuval.keidar <keidaryu@gmail.com>
2020-12-07 10:39:38 +0200

    update the logic to get the package name

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 currentVersion=$(npx -c 'echo "$npm_package_version"')
 repoName=$(npx -c 'echo "$npm_package_name"')
-packageNameSuffix="${repoName##*-}"
+repoPrefix="@playkit-js/playkit-js-"
+packageNameSuffix="${repoName#$repoPrefix}"
 echo $packageNameSuffix
 echo $currentVersion
 HTTPCODE=$(curl -k -d "{'name':$packageNameSuffix, 'version':$currentVersion, 'source':'npm', 'update_uiconf': $2}" -H "Content-Type: application/json" --silent --output /dev/stderr --write-out "%{http_code}" --fail -X POST https://jenkins.ovp.kaltura.com/generic-webhook-trigger/invoke?token=$1)
