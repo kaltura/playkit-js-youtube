1a0ebff2874a8f42a1267611fed2d5d92b416342
yuval.keidar <keidaryu@gmail.com>
2020-12-08 12:03:55 +0200

    add retry for curl request

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
+REPO_PREFIX="@playkit-js/playkit-js-"
+HTTP_SUCCESS=false
+
 currentVersion=$(npx -c 'echo "$npm_package_version"')
 repoName=$(npx -c 'echo "$npm_package_name"')
-repoPrefix="@playkit-js/playkit-js-"
-packageNameSuffix="${repoName#$repoPrefix}"
-echo $packageNameSuffix
-echo $currentVersion
-HTTPCODE=$(curl -k -d "{'name':$packageNameSuffix, 'version':$currentVersion, 'source':'npm', 'update_uiconf': $2}" -H "Content-Type: application/json" --silent --output /dev/stderr --write-out "%{http_code}" --fail -X POST https://jenkins.ovp.kaltura.com/generic-webhook-trigger/invoke?token=$1)
-STATUSCODE=$?
-if [ $HTTPCODE -ne 200 ] || [ $STATUSCODE -ne 0 ]; then
+packageNameSuffix="${repoName#$REPO_PREFIX}"
+echo "$packageNameSuffix"
+echo "$currentVersion"
+
+for i in {1..3}; do
+  echo "Try number $i for pinging Jenkins...\n"
+  HTTP_CODE=$(curl -k -d "{'name':$packageNameSuffix, 'version':$currentVersion, 'source':'npm', 'update_uiconf': $2}" -H "Content-Type: application/json" --silent --output /dev/stderr --write-out "%{http_code}" --fail -X POST https://jenkins.ovp.kaltura.com/generic-webhook-trigger/invoke?token=$1)
+  STATUS_CODE=$?
+  if [ "$HTTP_CODE" -eq 200 ] && [ "$STATUS_CODE" -eq 0 ]; then
+    HTTP_SUCCESS=true
+    break
+  fi
+done
+
+echo "Jenkins ping success status - $HTTP_SUCCESS"
+
+if [ "$HTTP_SUCCESS" = true ]; then
+  exit 0
+else
   exit 1
 fi
