792fa67c26f4acc429b963f1c2df48e5981e9506
Yuval Keidar <48407737+Yuvalke@users.noreply.github.com>
2020-12-29 09:45:43 +0200

    build(FEC-10700): Improvement for CI/CD (#35)
    add the number of commits from the master - added automatically by standard-version. (need static prefix and sha isn't needed anymore).
    handle two calls after deployment for each repo.
    releases from the no-master branch won’t be taken to beta/latest release.
    curl request check failure.
    change dependencies to work against the master for testing

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -12,22 +12,16 @@ elif [ "${TRAVIS_MODE}" = "release" ] || [ "${TRAVIS_MODE}" = "releaseCanary" ];
   if [ "${TRAVIS_MODE}" = "releaseCanary" ]; then
     echo "Run standard-version"
     yarn run release --prerelease canary --skip.commit=true --skip.tag=true
-    sha=$(git rev-parse --verify --short HEAD)
-    echo "Current sha ${sha}"
     currentVersion=$(npx -c 'echo "$npm_package_version"')
     echo "Current version ${currentVersion}"
-    newVersion=$(echo $currentVersion | sed -e "s/canary\.[[:digit:]]/canary.${sha}/g")
-    echo "New version ${newVersion}"
-    sed -iE "s/$currentVersion/$newVersion/g" package.json
-    sed -iE "s/$currentVersion/$newVersion/g" CHANGELOG.md
-    rm package.jsonE
-    rm CHANGELOG.mdE
   else
     conventional-github-releaser -p angular -t $GH_TOKEN
   fi
   echo "Building..."
   yarn run build
   echo "Finish building"
+elif [ "${TRAVIS_MODE}" = "deploy" ]; then
+  echo "Deploy..."
 else
 	echo "Unknown travis mode: ${TRAVIS_MODE}" 1>&2
 	exit 1
