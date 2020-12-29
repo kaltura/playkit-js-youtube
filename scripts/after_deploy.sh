c6bc725d4740b9408eff35b29cdedcceaa326f05
yuval.keidar <keidaryu@gmail.com>
2020-12-29 19:19:14 +0200

    build: incorrect branch on TRAVIS_BRANCH
    Issue: incorrect value in TRAVIS_BRANCH make an incorrect decision when we want to update the uiconf.
    Solution: use commit id to get the correct branch name from git.

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -8,10 +8,20 @@ packageNameSuffix="${repoName#$REPO_PREFIX}"
 echo "$packageNameSuffix"
 echo "$currentVersion"
 
+REAL_BRANCH=$(git ls-remote origin | sed -n "\|$TRAVIS_COMMIT\s\+refs/heads/|{s///p}")
+echo "$REAL_BRANCH"
+UPDATE_UI_CONF=true
+if [ "$REAL_BRANCH" = "master" ]; then
+  UPDATE_UI_CONF=true
+else
+  UPDATE_UI_CONF=false
+fi
+
 for i in {1..3}; do
   echo "Try number $i for pinging Jenkins...\n"
-  HTTP_CODE=$(curl -k -d "{'name':$packageNameSuffix, 'version':$currentVersion, 'source':'npm', 'update_uiconf': $2}" -H "Content-Type: application/json" --silent --output /dev/stderr --write-out "%{http_code}" --fail -X POST https://jenkins.ovp.kaltura.com/generic-webhook-trigger/invoke?token=$1)
+  HTTP_CODE=$(curl -k -d "{'name':$packageNameSuffix, 'version':$currentVersion, 'source':'npm', 'update_uiconf': $UPDATE_UI_CONF}" -H "Content-Type: application/json" --silent --output /dev/stderr --write-out "%{http_code}" --fail -X POST https://jenkins.ovp.kaltura.com/generic-webhook-trigger/invoke?token=$1)
   STATUS_CODE=$?
+  echo "Request return with http code $HTTP_CODE and curl finished with status code $STATUS_CODE"
   if [ "$HTTP_CODE" -eq 200 ] && [ "$STATUS_CODE" -eq 0 ]; then
     HTTP_SUCCESS=true
     break
